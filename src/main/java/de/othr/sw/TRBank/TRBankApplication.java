51e76bbd7843b3e98c462f010bf58d736d27b4b3
tobireith <tobias.reithmaier@st.oth-regensburg.de>
2022-01-18 20:52:35 +0100

    perform setup on run

--- src/main/java/de/othr/sw/TRBank/TRBankApplication.java
+++ src/main/java/de/othr/sw/TRBank/TRBankApplication.java
@@ -1,6 +1,5 @@
 package de.othr.sw.TRBank;
 
-import de.othr.sw.TRBank.service.KundeServiceIF;
 import de.othr.sw.TRBank.setup.SetupExecutor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -11,8 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class TRBankApplication {
+public class TRBankApplication implements ApplicationRunner {
+	@Autowired
+	SetupExecutor setupExecutor;
+
 	public static void main(String[] args) {
 		SpringApplication.run(TRBankApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		setupExecutor.executeSetup();
 	}
 }
