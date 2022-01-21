11fab00d8c5dd1201059676fb9245003696db217
tobireith <tobias.reithmaier@st.oth-regensburg.de>
2022-01-21 10:05:35 +0100

    Add planungsdoc, Adjust Usernames

--- src/main/java/de/othr/sw/TRBank/setup/KontoSetupPrivatkunde.java
+++ src/main/java/de/othr/sw/TRBank/setup/KontoSetupPrivatkunde.java
@@ -9,22 +9,22 @@ public class KontoSetupPrivatkunde extends KontoSetupAbstract{
     boolean setup() throws TRBankException {
         // Konten für Privatkunden hinzufügen
         if (!bankingService.kontoWithIbanExists("DE12345678901234500901")) {
-            Kunde kunde = kundeService.getKundeByUsername("customer1");
+            Kunde kunde = kundeService.getKundeByUsername("rennerRenate");
             bankingService.kontoAnlegen(new Konto("DE12345678901234500901", kunde, generateKontostandPrivatkunde()));
             generateRandomKonten(kunde);
         }
         if (!bankingService.kontoWithIbanExists("DE12345678901234500902")) {
-            Kunde kunde = kundeService.getKundeByUsername("customer2");
+            Kunde kunde = kundeService.getKundeByUsername("fischerBruno");
             bankingService.kontoAnlegen(new Konto("DE12345678901234500902", kunde, generateKontostandPrivatkunde()));
             generateRandomKonten(kunde);
         }
         if (!bankingService.kontoWithIbanExists("DE12345678901234500903")) {
-            Kunde kunde = kundeService.getKundeByUsername("customer3");
+            Kunde kunde = kundeService.getKundeByUsername("hofmeisterAngela");
             bankingService.kontoAnlegen(new Konto("DE12345678901234500903", kunde, generateKontostandPrivatkunde()));
             generateRandomKonten(kunde);
         }
         if (!bankingService.kontoWithIbanExists("DE12345678901234500904")) {
-            Kunde kunde = kundeService.getKundeByUsername("customer4");
+            Kunde kunde = kundeService.getKundeByUsername("bauerPeter");
             bankingService.kontoAnlegen(new Konto("DE12345678901234500904", kunde, generateKontostandPrivatkunde()));
             generateRandomKonten(kunde);
         }
